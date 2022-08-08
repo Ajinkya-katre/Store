9406f4c983c93c68a022b7dc55d0ea030f6a7860
ajinkya katre <katreajinkya@gmail.com>
2022-08-08 20:42:50 +0530

    files to be added

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from http.client import HTTPResponse
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse('This is home page')
+
+def about(request):
+    return HttpResponse('This is about page')
+
+def services(request):
+    return HttpResponse('This is services page')
+
+def contact(request):
+    return HttpResponse('This is contact page')
