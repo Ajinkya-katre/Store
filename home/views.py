79d9266119ca101a0e0f37a67e627a8078e382c6
ajinkya katre <katreajinkya@gmail.com>
2022-08-11 14:55:49 +0530

    contact form created

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -15,5 +17,12 @@ def services(request):
     return HttpResponse('This is services page')
 
 def contact(request):
+    if request.method == "post":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('msg')
+        contact = Contact(name=name, email=email, phone=phone, desc=msg, date=datetime.today())
+        contact.save()
     return render(request, 'contact.html')
     return HttpResponse('This is contact page')
