c2781a2159643a3d1552d5689d35bc45265e269a
ajinkya katre <katreajinkya@gmail.com>
2022-08-14 20:52:24 +0530

    Working on alert message

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from http.client import HTTPResponse
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -24,5 +25,7 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
         contact.save()
+        messages.success(request, 'Form is uploaded.')
+
     return render(request, 'contact.html')
     return HttpResponse('This is contact page')
